cbfe8f70fa361c38c9fd36087684fb766f0d766f
Yogesh Poul <yogeshpoul9999@gmail.com>
2026-06-27 11:19:36 +0530

    Add ping route

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
+
 // -------------------------------------------------
 // Admin routes
 // -------------------------------------------------
